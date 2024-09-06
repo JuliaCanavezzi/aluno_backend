5d86fe37521df85c1b44d476e6d839710155726f
JuliaCanavezzi <jucanavezzi03@gmail.com>
2024-09-06 14:08:36 -0300

    first commit

---
+++ src/main/java/aluno_backend/aluno_backend/AlunoBackendApplication.java
@@ -0,0 +1,13 @@
+package aluno_backend.aluno_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlunoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlunoBackendApplication.class, args);
+	}
+
+}
