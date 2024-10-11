7aa3038086d16e523611265706d05d1682356890
JuliaCanavezzi <jucanavezzi03@gmail.com>
2024-10-11 14:27:54 -0300

    Location para Salvar novo aluno (Nivel 2)

--- src/main/java/aluno_backend/aluno_backend/resources/AlunoController.java
+++ src/main/java/aluno_backend/aluno_backend/resources/AlunoController.java
@@ -1,11 +1,15 @@
 package aluno_backend.aluno_backend.resources;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
 import aluno_backend.aluno_backend.entities.Aluno;
 import aluno_backend.aluno_backend.services.AlunoService;
 
+import java.net.URI;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,18 +26,26 @@ public class AlunoController {
 
         
         @GetMapping("students")
-        public List<Aluno> getAlunos(){
-            return service.getAlunos(); //chama o serviço, devolve os alunos
+        public ResponseEntity <List<Aluno>> getAlunos(){
+            return ResponseEntity.ok(service.getAlunos()); //chama o serviço, devolve os alunos
         }
 
         @GetMapping("students/{id}")
-        public Aluno getAlunoById(@PathVariable int id){
-            return service.getAlunoById(id); 
+        public ResponseEntity <Aluno> getAlunoById(@PathVariable int id){
+            return ResponseEntity.ok(service.getAlunoById(id)); 
         }
 
         @PostMapping("students")
-        public Aluno save (@RequestBody Aluno aluno){
-            return service.save(aluno);
+        public ResponseEntity <Aluno> save (@RequestBody Aluno aluno){
+            Aluno novoAluno = service.save(aluno);
+
+            URI location = ServletUriComponentsBuilder
+                            .fromCurrentRequest()
+                            .path("/{id}")
+                            .buildAndExpand(novoAluno.getId())
+                            .toUri();
+
+            return ResponseEntity.created(location).body(novoAluno);
         }
 
         @DeleteMapping("students/{id}")
